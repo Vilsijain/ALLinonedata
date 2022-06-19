a99b1bd16879968792bfbb9c1cba55dc7ad6e29c
Vilsijain <93503754+Vilsijain@users.noreply.github.com>
2022-06-20 00:09:23 +0530

    Update aws_utils.py

--- aws_utils.py
+++ aws_utils.py
@@ -2,8 +2,8 @@ import os
 import boto3
 import botocore
 
-s3 = boto3.client('s3',aws_access_key_id=os.getenv(‘AWS_ACCESS_KEY_ID’),
-    aws_secret_access_key=os.getenv(‘AWS_SECRET_ACCESS_KEY’)
+s3 = boto3.client('s3',aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
 )
 def download_from_s3(bucket_name,key_name,download_file_name):
     try:
