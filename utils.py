b12cdc80d67672ca11ce38f935f2827c75fd8d46
Vilsijain <93503754+Vilsijain@users.noreply.github.com>
2022-06-20 15:26:27 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -69,6 +69,7 @@ def merge_words(word1,word2):
         for j in random.sample(result2,3):
             response.append(i+j)
     return response
+return result
 
 def generate_top_10(word):
     model = load_model()
