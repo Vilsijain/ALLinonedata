95c518f3bdbd748075f133cd8c4303d475270415
Vilsijain <93503754+Vilsijain@users.noreply.github.com>
2022-06-21 14:11:56 +0530

    Update aws_utils.py

--- aws_utils.py
+++ aws_utils.py
@@ -1,18 +1,16 @@
 import os
 import boto3
 import botocore
-
 s3 = boto3.resource('s3',aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
     aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
 )
 def download_from_s3(bucket_name,key_name,download_file_name):
     try:
-        s3 = boto3.resource('s3')
-        obj = s3.Object(bucket_name,download_file_name)
-        body = obj.get()['Body'].read()   
+        with open(f'{download_file_name}', 'wb') as data:
+            s3.download_fileobj(bucket_name, key_name, data)
+        return download_file_name
     except botocore.exceptions.ClientError as error:
         print(error)
     except Exception as e:
         print(e)
-
 
